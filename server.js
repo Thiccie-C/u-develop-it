e9e4864db4652bf4ca011bf6f0cb9437ac6667e1
Cameron Nelson <nelsonnoremac@gmail.com>
2022-02-08 17:47:44 -0600

    added to server.js

--- server.js
+++ server.js
@@ -19,8 +19,30 @@ const db = mysql.createConnection(
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
+// db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+//     if (err) {
+//       console.log(err);
+//     }
+//     console.log(row);
+//   });
+//   db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+//     if (err) {
+//       console.log(err);
+//     }
+//     console.log(result);
+//   });
 // Default response for any other request (Not Found)
+// Create a candidate
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
+              VALUES (?,?,?,?)`;
+const params = [1, 'Ronald', 'Firbank', 1];
+
+db.query(sql, params, (err, result) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log(result);
+});
 app.use((req, res) => {
   res.status(404).end();
 });
