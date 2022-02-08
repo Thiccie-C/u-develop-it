51deb027bfeca1c3d8dec4a9c8c3a307cfe8e7bd
Cameron Nelson <nelsonnoremac@gmail.com>
2022-02-08 17:32:41 -0600

    fixing branch

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
+const mysql = require('mysql2');
 const express = require('express');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: 'Cameron',
+      // Your MySQL password
+      password: 'P@ccraft1618',
+      database: 'election'
+    },
+    console.log('Connected to the election database.')
+  );
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
